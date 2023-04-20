85f89a6233093ccacacf01e7c03834f210458b42
Raviteja-bh <ravitejabh9790@gmail.com>
2023-04-20 16:40:19 +0530

    Bugs Fixed in the JavaScript

--- interactive.js
+++ interactive.js
@@ -56,6 +56,8 @@ function operator(firstNum, secondNum, operator) {
         console.log("Pick the Correct operator!");
     }
 }
+
+//variables for storing calculations.
 let firstNum = '';
 let operation = '';
 let secondNum = '';
@@ -72,7 +74,7 @@ let btn = Array.from(document.querySelectorAll('button'));
 btn.map(bt => {
     bt.addEventListener('click', () => {
 
-
+        // check if it is a number and add to first opparend.
         if (!isNaN(bt.innerText) && operation == '') {
             if (prevClick != '=') {
                 firstNum += bt.innerText;
@@ -80,27 +82,31 @@ btn.map(bt => {
             } else if (prevClick == '=') {
                 alert('Pick the correct operator');
             }
-            console.log("first" + " " + firstNum);
+            // console.log("first" + " " + firstNum);
+            //check if it is a number and add to second opparend.
         } else if (!isNaN(bt.innerText) && operation != '') {
             secondNum += bt.innerText;
             writeEle.innerText += bt.innerText;
-            console.log("second" + " " + secondNum);
+            // console.log("second" + " " + secondNum);
+            //check for . dot notation.
         } else if (bt.innerText == '.' && firstNum != '' && operation == '') {
             if (firstNum.includes('.')) {
                 alert('can\'t use more than one \'.\'');
             } else {
                 firstNum += bt.innerText;
                 writeEle.innerText += bt.innerText;
-                console.log("dot1" + firstNum);
+                // console.log("dot1" + firstNum);
             }
+            //check for .dot notation for second element.
         } else if (bt.innerText == '.' && secondNum != '' && operation != '') {
             if (secondNum.includes('.')) {
                 alert('can\'t use more than one \'.\'');
             } else {
                 secondNum += bt.innerText;
                 writeEle.innerText += bt.innerText;
-                console.log("dot" + secondNum);
+                // console.log("dot" + secondNum);
             }
+            //checks for operation symbol
         } else if (firstNum != '' && secondNum == '' && (bt.innerText == '+' || bt.innerText == '-'
             || bt.innerText == 'x' || bt.innerText == '/' || bt.innerText == '%')) {
             if (bt.innerText == 'x') {
@@ -111,7 +117,8 @@ btn.map(bt => {
                 writeEle.innerText += bt.innerText;
             }
             prevClick = '';
-            console.log("operation" + " " + operation);
+            // console.log("operation" + " " + operation);
+            // if the buttons is = then calls function for calculation.
         } else if (firstNum != '' && secondNum != '' && bt.innerText == '=') {
             let result = operator(parseFloat(firstNum), parseFloat(secondNum), operation);
             prevClick = bt.innerText;
@@ -131,9 +138,10 @@ btn.map(bt => {
                     writeEl1.innerText = result.toFixed(5);
                 };
             }
-            console.log(firstNum);
+            // console.log(firstNum);
             operation = '';
             secondNum = '';
+            //if the buttons is any operation then calls function for calculation and adds to firstNum
         } else if (firstNum != '' && secondNum != '' && (bt.innerText == '+' || bt.innerText == '-'
             || bt.innerText == 'x' || bt.innerText == '/' || bt.innerText == '%')) {
             let result = operator(parseInt(firstNum), parseInt(secondNum), operation);
@@ -162,8 +170,9 @@ btn.map(bt => {
                 writeEle.innerText += bt.innerText;
             }
             secondNum = '';
-            console.log(firstNum);
-            console.log(operation);
+            // console.log(firstNum);
+            // console.log(operation);
+            // if AC is clicked it clears all the calculations.
         } else if (bt.innerText == 'AC') {
             firstNum = '';
             secondNum = '';
@@ -171,15 +180,18 @@ btn.map(bt => {
             prevClick = '';
             writeEle.innerText = '';
             writeEl1.innerText = '';
+            //if C is clicked removes last letter from calculation of firstNum.
         } else if (bt.innerText == 'C' && firstNum != '' && operation == '') {
             firstNum = firstNum.substring(0, firstNum.length - 1);
             writeEle.innerText = firstNum;
-            console.log(firstNum);
+            // console.log(firstNum);
+            //if C is clicked removes last letter from calculation fo secondNum.
         } else if (bt.innerText == 'C' && secondNum != '') {
             secondNum = secondNum.substring(0, secondNum.length - 1);
             let ans = writeEle.innerText;
             writeEle.innerText = ans.substring(0, ans.length - 1);
-            console.log(secondNum);
+            // console.log(secondNum);
+            // if +/- is clicked then adds '-' to number.
         } else if (bt.innerText == '+/-' && firstNum != '' && operation == '') {
             if (firstNum.includes('-')) {
                 firstNum = firstNum.substring(1);
@@ -188,6 +200,7 @@ btn.map(bt => {
                 firstNum = '-' + writeEle.innerText
                 writeEle.innerText = firstNum;
             }
+            // if +/- clicked then adds '-' to second number.
         } else if (bt.innerText == '+/-' && secondNum != '' && operation != '') {
             if (secondNum.includes('-')) {
                 secondNum = secondNum.substring(1);
@@ -195,7 +208,7 @@ btn.map(bt => {
             } else {
                 secondNum = '-' + secondNum;
                 writeEle.innerText = firstNum + operation + secondNum;
-                console.log(secondNum);
+                // console.log(secondNum);
             }
         }
     })
